62f81e0d05859a3ffe657353815500567a341ba5
Zakhar Antoshchenko <zakhar.antoschenko@gmail.com>
2019-02-19 11:55:41 +0200

    fixed a secret key for compatibility with rails < 5.2.0

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,13 +1,19 @@
 module JsonWebToken
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, Rails.application.credentials.secret_key_base)
+    JWT.encode(payload, secret_key)
   end
 
   def self.decode(token)
-    body = JWT.decode(token, Rails.application.credentials.secret_key_base)[0]
+    body = JWT.decode(token, secret_key)[0]
     HashWithIndifferentAccess.new body
   rescue
     nil
+  end
+
+  private
+
+  def secret_key
+    Rails.application.secrets&.secret_key_base || Rails.application.credentials&.secret_key_base
   end
 end
